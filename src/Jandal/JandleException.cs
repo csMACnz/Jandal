44eac6fc9eba1022f34e9eacddae0fcc83e7f052
Mark Clearwater <csmacnz@csmac.co.nz>
2016-06-23 19:33:05 +0100

    initial code commit

---
+++ src/Jandal/JandleException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Handle.The
+{
+    public class JandleException : Exception
+    {
+        [DebuggerHidden]
+        public JandleException()
+        {
+            Environment.FailFast("You can't handle it!", this);
+        }
+    }
+}
